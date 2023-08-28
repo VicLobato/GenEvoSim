4e0426cf81de4bd807d62e1eccecb6cf4125a208
Victor <vlobatogarcia@gmail.com>
2023-08-28 11:16:25 +0100

    give up on workflows

--- build/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Change directory to build
-cd build
-
-# Build options using local SFML
-g++ -c ../main.cpp -I../SFML-2.6.0/include
-g++ main.o -o smfl-app -L../SFML-2.6.0/lib -lsfml-graphics -lsfml-window -lsfml-system
